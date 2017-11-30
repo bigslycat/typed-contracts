cfef64ddc110059e86653924557d2f784647e3a3
Pavel <me@pavlik.pro>
2017-11-30 05:57:12 +0300

    Add isLiteral contract

---
+++ __tests__/contracts/literal.js
@@ -0,0 +1,23 @@
+/* @flow */
+
+import { isLiteral } from '../../src/contracts/literal'
+import { ValidationError } from '../../src/ValidationError'
+
+describe('isLiteral', () => {
+  describe('Creates new Contract for string or number specific value', () => {
+    it('Returns value if value equals expected value', () => {
+      expect(isLiteral('expected value')('valueName', 'expected value'))
+        .toBe('expected value')
+    })
+
+    it('Returns ValidationError instance in other cases', () => {
+      expect(isLiteral('expected value')('valueName', 'invalid value'))
+        .toBeInstanceOf(ValidationError)
+    })
+
+    it('expectedTypes property of ValidationError equals [\'"<expected value>"\']', () => {
+      expect((isLiteral('expected value')('valueName', 'invalid value'): any).expectedTypes)
+        .toEqual(['"expected value"'])
+    })
+  })
+})
