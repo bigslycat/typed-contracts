97c08e7f71f41316ae46bd2e04aa50611bed78de
Pavel <me@pavlik.pro>
2017-11-30 14:56:38 +0300

    Add index file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* @flow */
+
+export * from './contracts/array'
+export * from './contracts/literal'
+export * from './contracts/null'
+export * from './contracts/number'
+export * from './contracts/object'
+export * from './contracts/string'
+export * from './contracts/union'
+export * from './contracts/void'
+
+export * from './createContract'
+export * from './ValidationError'
