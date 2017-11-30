3a5f42187b5746552416806d4ec28595fc078c19
Pavel <me@pavlik.pro>
2017-11-30 14:55:49 +0300

    Add isUnion contract

---
+++ __tests__/contracts/union.js
@@ -0,0 +1,38 @@
+/* @flow */
+
+import { isUnion } from '../../src/contracts/union'
+import { ValidationError } from '../../src/ValidationError'
+
+const createError =
+  (...types: string[]) =>
+    (name: string, value: mixed) =>
+      ValidationError.of(name, value, ...types)
+
+describe('isUnion', () => {
+  describe('Creates new Contract for one or more other contracts or validation functions', () => {
+    it('Return ValidationError if all Contract returns ValidationError', () => {
+      const validate1 = jest.fn(createError('type1'))
+      const validate2 = jest.fn(createError('type2'))
+      const validate3 = jest.fn(createError('type3'))
+
+      const result = isUnion(validate1, validate2, validate3)('valueName', 'value')
+
+      expect(result).toBeInstanceOf(ValidationError)
+      expect(result.expectedTypes).toEqual(['type1', 'type2', 'type3'])
+      expect(validate1).lastCalledWith('valueName', 'value')
+      expect(validate2).lastCalledWith('valueName', 'value')
+      expect(validate3).lastCalledWith('valueName', 'value')
+    })
+
+    it('Return value in other cases', () => {
+      const validate1 = jest.fn().mockReturnValue('result 1')
+      const validate2 = jest.fn(createError('type1'))
+      const validate3 = jest.fn(createError('type2'))
+
+      expect(isUnion(validate1, validate2, validate3)('valueName', 'value')).toBe('value')
+      expect(validate1).lastCalledWith('valueName', 'value')
+      expect(validate2).lastCalledWith('valueName', 'value')
+      expect(validate3).lastCalledWith('valueName', 'value')
+    })
+  })
+})
---
+++ src/contracts/union.js
@@ -0,0 +1,35 @@
+/* @flow */
+
+import { ValidationError } from '../ValidationError'
+import { createContract, type Contract } from '../createContract'
+
+type UnionContract =
+  <T>(...contracts: Array<(name: string, value: mixed) => ValidationError | T>) =>
+    Contract<T>
+
+export const isUnion: UnionContract =
+  /* :: <T> */(...contracts: Array<(name: string, value: mixed) => ValidationError | T>): any => {
+    const unionContract = (name, value: any): ValidationError | T => {
+      const errors = contracts
+        .map(contract => contract(name, value))
+        .reduce((scope, error): ValidationError[] => {
+          if (error instanceof ValidationError) scope.push(error)
+          return scope
+        }, [])
+
+      if (errors.length !== contracts.length) return value
+
+      const types = errors.reduce(
+        (scope, { expectedTypes }): string[] => {
+          for (let i = 0; i < expectedTypes.length; i += 1) {
+            scope.push(expectedTypes[i])
+          }
+          return scope
+        }, [],
+      )
+
+      return (ValidationError.of: any)(name, value, ...types)
+    }
+
+    return createContract(unionContract)
+  }
