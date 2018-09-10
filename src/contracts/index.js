5a5651a2d8d55d5c19d2721f20a541bc81e808b8
Pavel Tereschenko <me@stupidcat.wtf>
2018-09-10 23:56:40 +0300

    Refactoring

---
+++ src/contracts/index.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+export * from './array';
+export * from './boolean';
+export * from './literal';
+export * from './null';
+export * from './number';
+export * from './object';
+export * from './string';
+export * from './union';
+export * from './void';
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 /* @flow */
 
-export * from './contracts/array';
-export * from './contracts/literal';
-export * from './contracts/null';
-export * from './contracts/number';
-export * from './contracts/boolean';
-export * from './contracts/object';
-export * from './contracts/string';
-export * from './contracts/union';
-export * from './contracts/void';
-
-export { createContract } from './createContract';
-export type { Contract } from './createContract';
-export { ValidationError } from './ValidationError';
+export * from './contracts';
+export * from './createContract';
+export * from './ValidationError';
