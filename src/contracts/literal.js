f77499dcfcbf15220a9f5e74abfb9da870d2c1eb
Pavel Tereschenko <me@stupidcat.wtf>
2018-09-12 21:21:56 +0300

    Update literal

--- src/contracts/literal.js
+++ src/contracts/literal.js
@@ -1,17 +1,15 @@
 /* @flow */
 
 import { ValidationError } from '../ValidationError';
-import { createContract, type Contract } from '../createContract';
+import * as contract from '../Contract';
 
 export const literal = <T: string | number | boolean>(
-  expected: T,
-): Contract<T> =>
-  createContract(
-    (valueName, value: mixed): any => {
-      if (value === expected) return value;
-      return ValidationError.of(valueName, value, JSON.stringify(expected));
-    },
-  );
+  expectedValue: T,
+): contract.Contract<T> =>
+  contract.of((valueName, value) => {
+    if (value === expectedValue) return (value: any);
+    return new ValidationError(valueName, value, JSON.stringify(expectedValue));
+  });
 
 export const isLiteral = literal;
 export const passLiteral = literal;
