4689ecb77c21e0ceb48222aa813784cac0c2a82b
Pavel Tereschenko <me@stupidcat.wtf>
2018-09-12 21:46:53 +0300

    Fix object

--- src/contracts/object/object.js
+++ src/contracts/object/object.js
@@ -3,8 +3,6 @@
 import { ValidationError } from '../../ValidationError';
 import { createContract, type Contract } from '../../createContract';
 
-import { handler } from './handler';
-
 type ExtractReturnType = <T>(
   (valueName: string, value: any) => ValidationError | T,
 ) => $Supertype<T>;
@@ -13,7 +11,34 @@ export const object = <
   S: { [prop: string]: (valueName: string, value: mixed) => any },
 >(
   spec: S,
-): Contract<$ObjMap<S, ExtractReturnType>> => createContract(handler(spec));
+): Contract<$ObjMap<S, ExtractReturnType>> =>
+  createContract((valueName, value) => {
+    if (!value || typeof value != 'object' || Array.isArray(value)) {
+      return new ValidationError(valueName, value, 'Object');
+    }
+
+    const errors = Object.entries(spec).reduce(
+      (acc, [key, validate]: [string, any]) => {
+        const result = validate(`${valueName}.${key}`, value[key]);
+
+        if (result instanceof ValidationError) acc.push(result);
+
+        return acc;
+      },
+      [],
+    );
+
+    return errors.length
+      ? // $FlowFixMe
+        new ValidationError(
+          valueName,
+          value,
+          ...errors
+            .map(error => error.expectedTypes)
+            .reduce((prev, next) => prev.concat(next), []),
+        )
+      : value;
+  });
 
 export const isObject = object;
 export const passObject = object;
--- src/contracts/object/objectOf.js
+++ src/contracts/object/objectOf.js
@@ -3,9 +3,7 @@
 import { ValidationError } from '../../ValidationError';
 import { createContract, type Contract } from '../../createContract';
 
-import { literal } from '../literal';
-
-import { handler } from './handler';
+import { union } from '../union';
 
 type ObjectOf = <T, L: string | number | boolean>(
   ...rules: $ReadOnlyArray<
@@ -17,15 +15,27 @@ export const objectOf: ObjectOf = <T, L: string | number | boolean>(
   ...rules: $ReadOnlyArray<
     ((name: string, value: mixed) => ValidationError | T) | L,
   >
-): Contract<{ [string]: T | L }> =>
-  createContract(
-    handler(
-      Object.entries(rules).reduce((acc, [key, rule]: any) => {
-        acc[key] = typeof rule == 'function' ? rule : literal(rule);
+): Contract<{ [string]: T | L }> => {
+  const validate = union(...rules);
+
+  return createContract(
+    (valueName, value): any => {
+      if (!value || typeof value != 'object' || Array.isArray(value)) {
+        return new ValidationError(valueName, value, 'Object');
+      }
+
+      const errors = Object.entries(value).reduce((acc, [key, val]) => {
+        const result = validate(`${valueName}.${key}`, val);
+        if (result instanceof ValidationError) acc.push(result);
         return acc;
-      }, {}),
-    ),
+      }, []);
+
+      return errors.length
+        ? new ValidationError(valueName, value, ...errors[0].expectedTypes)
+        : value;
+    },
   );
+};
 
 export const isObjectOf = objectOf;
 export const passObjectOf = objectOf;
