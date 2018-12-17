95881a635000b325081f0f83ffdb3ef8769b3b7c
Pavel Tereschenko <me@stupidcat.wtf>
2018-12-17 14:21:53 +0500

    Fix shape

--- src/contracts/object/shape.js
+++ src/contracts/object/shape.js
@@ -7,17 +7,16 @@ import { object } from './object';
 
 type ExtractReturnType = <T>(
   (valueName: string, value: mixed) => ValidationError | void | T,
-) => $Supertype<T>;
+) => T;
 
-export const shape = <
+export function shape<
   S: { [prop: string]: (valueName: string, value: mixed) => any },
->(
-  spec: S,
-): contract.Contract<$ObjMap<S, ExtractReturnType>> =>
-  object(
+>(spec: S): contract.Contract<$ReadOnly<$ObjMap<S, ExtractReturnType>>> {
+  return object(
     Object.entries(spec).reduce((acc, [key, validate]: any) => {
       acc[key] = (valueName, value) =>
         value === undefined ? value : validate(valueName, value);
       return acc;
     }, {}),
   );
+}
